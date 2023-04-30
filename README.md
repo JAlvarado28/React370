d349a05279444a00d127c1dab04c8494d14d3e69
Jesus Alvarado <jalvarado28@csuchico.edu>
2023-04-30 10:42:22 -0700

    Fixed the backend, just needs the CSS to look better

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 For reference, video [React Website Tutorial] (https://www.youtube.com/watch?v=I2UBjN5ER4s)
 
+His github link (https://github.com/briancodex/react-website-v1/tree/master/src/components)
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/ViewDatabase.js
+++ src/components/ViewDatabase.js
@@ -1,20 +1,76 @@
-import React from "react";
-// import {Button} from "./Button";
-// import {Link} from "react-router-dom";
+import React, { useState } from "react";
 import "./ViewDatabase.css";
-import backgroundImage from "../imagesSRC/porsche.jpg";
+import backgroundImage from "../imagesSRC/screen.jpg";
+import ViewDatabaseModal from "./ViewSQLmod.js";
+import { Button } from "./Button.js";
 
 function ViewDatabase() {
-    const backgroundImageStyle = {
-        backgroundImage: `url(${backgroundImage})`,
-    };
-    return (
-        <div className="viewDatabase-container" style={backgroundImageStyle}>
-            <h1>View Database</h1>
-            <p>This where the data will be at! Just wait! Please!</p>
-        </div>
+  // for database query
+  const [data, setData] = useState([]);
+  const [tableName, setTableName] = useState("");
+  const [showModal, setShowModal] = useState(false);
 
-    );
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  // allows the image to be displayed
+  const backgroundImageStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+  };
+
+  // displays the data
+  const renderData = () => {
+    return data.map((row, index) => {
+      return (
+        <tr key={index}>
+          {Object.values(row).map((value, index) => {
+            return <td key={index}>{value}</td>;
+          })}
+        </tr>
+      );
+    });
+  };
+
+  return (
+    <div className="viewDatabase-container" style={backgroundImageStyle}>
+        <div className='modifyDatabase-header'>
+      <h1>Transactions</h1>
+      <p>Use Select</p>
+      </div>
+      <div className='modifyDatabase-btns'>
+        <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large' onClick={openModal}>
+            Transaction </Button> 
+      <ViewDatabaseModal
+        show={showModal}
+        handleClose={closeModal}
+        setData={(data, tableName) => {
+        setData(data);
+        setTableName(tableName);
+        }}
+        route="/execute-select-sql"
+      />
+      </div>
+      <div className="table-title"> Table: {tableName}  </div>
+      {data.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              {Object.keys(data[0]).map((key, index) => {
+                return <th key={index}>{key}</th>;
+              })}
+            </tr>
+          </thead>
+          <tbody>{renderData()}</tbody>
+        </table>
+      )}
+    </div>
+  );
 }
 
-export default ViewDatabase
+export default ViewDatabase;
